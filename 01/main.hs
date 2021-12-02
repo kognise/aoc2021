428770e2e5860c36ac658543f509b1c32a1846c9
Kognise <felix.mattick@gmail.com>
2021-12-02 00:38:38 -0500

    Slightly tweak day 1

--- 01/main.hs
+++ 01/main.hs
@@ -3,5 +3,5 @@ import Data.List (tails);
 windows n xs = take (length xs - n + 1) $ map (take n) (tails xs)
 
 main = interact $ \input ->
-  -- show . length . filter (\[a, b] -> b > a) . windows 2 . map (\i -> read i :: Int) $ lines input
-  show . length . filter (\[a, b] -> b > a) . windows 2 . map sum . windows 3 . map read $ lines input
+  -- show $ length $ filter (\[a, b] -> b > a) $ windows 2 $ map (\i -> read i :: Int) $ lines input
+  show $ length $ filter (\[a, b] -> b > a) $ windows 2 $ map sum $ windows 3 $ map read $ lines input
