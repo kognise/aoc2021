fadb011d5c8cfe39cd1230438640fc82977a0254
Kognise <felix.mattick@gmail.com>
2021-12-05 14:18:00 -0500

    UwU

--- 05/main.js
+++ 05/main.js
@@ -3,32 +3,26 @@ const lines = text.split('\n').map((line) => line.split(' -> ').map((side) => si
 
 const plane = {}
 
-for (const [ [xa1, ya1], [xb1, yb1] ] of lines) {
-    const [xa, ya] = [Math.min(xa1, xb1), Math.min(ya1, yb1)]
-    const [xb, yb] = [Math.max(xa1, xb1), Math.max(ya1, yb1)]
+for (const [ [xa, ya], [xb, yb] ] of lines) {
+    const [xMin, yMin] = [Math.min(xa, xb), Math.min(ya, yb)]
+    const [xMax, yMax] = [Math.max(xa, xb), Math.max(ya, yb)]
 
-    if (xa === xb) {
-        for (let y = ya; y <= yb; y++) {
-            plane[`${xa},${y}`] = plane[`${xa},${y}`] ? plane[`${xa},${y}`] + 1 : 1
-        }
-    } else if (ya === yb) {
-        for (let x = xa; x <= xb; x++) {
-            plane[`${x},${ya}`] = plane[`${x},${ya}`] ? plane[`${x},${ya}`] + 1 : 1
+    if (xMin === xMax) {
+        for (let y = yMin; y <= yMax; y++) {
+            plane[`${xMin},${y}`] = plane[`${xMin},${y}`] ? plane[`${xMin},${y}`] + 1 : 1
         }
     } else {
-        // Handle diagonal lines
-        const slope = (yb1 - ya1) / (xb1 - xa1)
-        const intercept = ya1 - slope * xa1
-        for (let x = xa; x <= xb; x++) {
+        const slope = (yb - ya) / (xb - xa)
+        const intercept = ya - slope * xa
+        for (let x = xMin; x <= xMax; x++) {
             const y = Math.round(slope * x + intercept)
             plane[`${x},${y}`] = plane[`${x},${y}`] ? plane[`${x},${y}`] + 1 : 1
         }
     }
 }
 
-let number = 0
-for (const [_, value] of Object.entries(plane)) {
-    if (value >= 2) number++
+let count = 0
+for (const value of Object.values(plane)) {
+    if (value >= 2) count++
 }
-
-console.log(number)
+console.log(count)
